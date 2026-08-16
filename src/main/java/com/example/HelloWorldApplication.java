21e54e287d06fff34322f034f989a394beb10707
Ubuntu <ubuntu@ip-172-31-38-89.ap-south-1.compute.internal>
2026-08-16 05:41:55 +0000

    modified files

--- src/main/java/com/example/HelloWorldApplication.java
+++ src/main/java/com/example/HelloWorldApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HelloWorld {
+public class HelloWorldApplication{
 
     public static void main(String[] args) {
         SpringApplication.run(HelloWorldApplication.class, args);
